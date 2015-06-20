709643f385917f73e2f0985c0ad349aea6ae5ebc
yaredtek <yaredtek@gmail.com>
2015-06-20 01:53:40 -0400

    refactoring

--- YaredPersonalWebSite/App_Start/BundleConfig.cs
+++ YaredPersonalWebSite/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace YaredPersonalWebSite
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -22,6 +24,24 @@ namespace YaredPersonalWebSite
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/UI/assets/js/modernizr.custom.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/UI/assets/js/jquery-1.11.1.min.js",
+                        "~/UI/assets/bootstrap/js/bootstrap.min.js",
+                        "~/UI/assets/js/jquery.parallax-1.1.3.js",
+                        "~/UI/assets/js/imagesloaded.pkgd.js",
+                        "~/UI/assets/js/jquery.sticky.js",
+                        "~/UI/assets/js/smoothscroll.js",
+                        "~/UI/assets/js/wow.min.js",
+                        "~/UI/assets/js/jquery.easypiechart.js",
+                        "~/UI/assets/js/waypoints.min.js",
+                        "~/UI/assets/js/jquery.cbpQTRotator.js",
+                        "~/UI/assets/js/custom.js"));
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
@@ -38,14 +58,15 @@ namespace YaredPersonalWebSite
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
-     
-            bundles.Add(new StyleBundle("~/global/css").Include(
-                        "~/UI/assets/bootstrap/css/bootstrap.min.css",
+
+            bundles.Add(new StyleBundle("~/UI/assets/bootstrap/css/css").Include("~/UI/assets/bootstrap/css/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/UI/assets/css/css").Include(
                         "~/UI/assets/css/font-awesome.min.css",
                         "~/UI/assets/css/simple-line-icons.css",
-                        "~/UI/assets/css/animate.css"));
+                        "~/UI/assets/css/animate.css",
+                        "~/UI/assets/css/style.css"));
 
-            bundles.Add(new StyleBundle("~/home/css").Include("~/UI/assets/css/style.css"));
 
         }
     }
