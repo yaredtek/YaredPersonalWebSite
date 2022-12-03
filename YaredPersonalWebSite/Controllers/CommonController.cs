33d61689c7c7d8d71e029f6fe9089524f647cd76
Yared <yaredtek@gmail.com>
2022-12-02 23:50:32 -0500

    Refactoring

---
+++ YaredPersonalWebSite/Controllers/CommonController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YaredPersonalWebSite.Controllers
+{
+    public class CommonController : Controller
+    {
+        [HttpPost]
+        public ActionResult SendMail(Models.MailModel form)
+        {
+            var retValue = string.Empty;
+            if (!ModelState.IsValid)
+            {
+                return Content(retValue);
+            }
+
+            if (!ModelState.IsValid) return Content(retValue);
+
+            //Update your SMTP server credentials
+            retValue = "There was an error submitting the form, please try again later.";
+            using (var client = new SmtpClient
+            {
+                Host = ConfigurationManager.AppSettings["SmtpServerName"],
+                Port = 25,
+                EnableSsl = false,
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            })
+            {
+                var mail = new MailMessage();
+                mail.To.Add(ConfigurationManager.AppSettings["ToEmailAddress"]);
+                mail.From = new MailAddress(form.Email, form.Name);
+                mail.Subject = String.Format(form.Subject);
+                mail.Body = form.Message + "<br/><br/>" + form.Email;
+                mail.IsBodyHtml = true;
+                client.Send(mail);
+                retValue = "Message Sent. We will contact you ASAP. Thanks.";
+            }
+            return Content(retValue);
+
+        }
+
+    }
+}
---
+++ YaredPersonalWebSite/Models/MailModel.cs
@@ -0,0 +1,28 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace YaredPersonalWebSite.Models
+{
+    public class MailModel
+    {
+        [Required(ErrorMessage = "Subject is required")]
+        public  string Subject { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        [Email(ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+
+ 
+    }
+}
